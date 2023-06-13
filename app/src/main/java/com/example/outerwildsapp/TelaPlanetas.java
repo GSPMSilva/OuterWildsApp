6c5cbd8d0f2f50f1101be16a02f6453ba680bbbb
yGabrielT <gtrindade380@gmail.com>
2023-06-12 21:44:17 -0300

    Termino da activity_tela_planetas.xml.

--- app/src/main/java/com/example/outerwildsapp/TelaPlanetas.java
+++ app/src/main/java/com/example/outerwildsapp/TelaPlanetas.java
@@ -2,7 +2,9 @@ package com.example.outerwildsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaPlanetas extends AppCompatActivity {
 
@@ -11,4 +13,35 @@ public class TelaPlanetas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_planetas);
     }
+
+    public void IrAbrolho (View view){
+        Intent intent = new Intent(this, planeta_abrolho.class);
+        startActivity(intent);
+
+    }
+
+    public void IrProfundezas (View view){
+        Intent intent = new Intent(this, planeta_profundezas.class);
+        startActivity(intent);
+
+    }
+
+    public void IrVale (View view){
+        Intent intent = new Intent(this, planeta_vale.class);
+        startActivity(intent);
+
+    }
+
+    public void IrRecanto (View view){
+        Intent intent = new Intent(this, planeta_recanto.class);
+        startActivity(intent);
+
+    }
+
+    public void Voltar (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
---
+++ app/src/main/java/com/example/outerwildsapp/planeta_recanto.java
@@ -0,0 +1,14 @@
+package com.example.outerwildsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class planeta_recanto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planeta_recanto);
+    }
+}
