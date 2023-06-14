2151650dbf63365730ea5e527f6ee5d02cf310cc
yGabrielT <gtrindade380@gmail.com>
2023-06-14 17:35:24 -0300

    Adição da musica do jogo no menu principal.

--- app/src/main/java/com/example/outerwildsapp/MainActivity.java
+++ app/src/main/java/com/example/outerwildsapp/MainActivity.java
@@ -3,19 +3,53 @@ package com.example.outerwildsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+    ImageButton play;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play = findViewById(R.id.imageButtonPlay);
 
 
+
+    }
+
+    public void MusicaPlay (View view){
+        /** Inicia musica e troca para imagem de pausa**/
+        if(mediaPlayer == null ){
+            mediaPlayer =  MediaPlayer.create(this,(R.raw.music_outer));
+
+        }
+
+        /** Caso esteja tocando uma musica e se clica o botão: pausa e troca imagem para o play
+         * senão a toca **/
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            play.setImageResource(R.drawable.img_play);
+        }
+        else{
+            mediaPlayer.start();
+            play.setImageResource(R.drawable.img_pause);
+        }
+
+
+        /** Quando a musica terminar para a musica e troca a imagem para o play**/
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mediaPlayer){
+                mediaPlayer.stop();
+                play.setImageResource(R.drawable.img_play);
+            }
+        });
 
 
     }
